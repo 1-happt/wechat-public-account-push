b70b31b563e304a6383103dc973a2cd7a67affd6
1-happt <121279271+1-happt@users.noreply.github.com>
2022-12-23 14:36:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,44 +8,38 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: ' wx4d7b00c15026686d',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '433e399cc3771fbd3a9ece5f66f5a7ab',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '云南',
+  CITY: '楚雄',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '亲爱的晓娟',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'omfNg5_d8P7_19CAo03mlB0CbyPE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: ' zlOLyoU2GPRFAvtLI2lU8QjBPRDF4nrVTv8N5-ZAaBE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-26',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '晓娟', year: '1996', date: '10-26',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '1996', date: '09-18',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '05-07',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
+      
     },
   ],
 
